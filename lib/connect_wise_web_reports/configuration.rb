3b695d4e6640a170e94a83cd569e31409c3ea55c
Kevin Pheasey <kevin@kpheasey.com>
2016-11-03 14:07:41 -0400

    Add a logger

--- lib/connect_wise_web_reports/configuration.rb
+++ lib/connect_wise_web_reports/configuration.rb
@@ -26,4 +26,14 @@ module ConnectWiseWebReports
 
   end
 
+  class << self
+    attr_writer :logger
+
+    def logger
+      @logger ||= Logger.new($stdout).tap do |log|
+        log.progname = self.name
+      end
+    end
+  end
+
 end
